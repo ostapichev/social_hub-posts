92861bd0f3bc1d403b73de5e20e3657a0927a9fa
olehlevik <ytox@ukr.net>
2024-08-28 01:04:10 +0300

    Authorisation part 2

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,12 +1,16 @@
 import { ConflictException, Injectable, Logger } from '@nestjs/common';
 
+import { UserEntity } from '../../database/entities/user.entity';
+import { IUserData } from '../auth/interfaces/user-data.interface';
 import { LoggerService } from '../logger/logger.service';
+import { PostsService } from '../posts/posts.service';
 import { UserRepository } from '../repository/services/user.repository';
 import { UpdateUserDto } from './dto/req/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
+    private readonly carsService: PostsService,
     private readonly logger: LoggerService,
     private readonly userRepository: UserRepository,
   ) {}
@@ -15,12 +19,15 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  public async findMe(id: number): Promise<any> {
-    return `This action returns a #${id} user`;
+  public async findMe(userData: IUserData): Promise<UserEntity> {
+    return await this.userRepository.findOneBy({ id: userData.userId });
   }
 
-  public async updateMe(id: number, dto: UpdateUserDto): Promise<any> {
-    Logger.log(dto);
+  public async updateMe(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<any> {
+    Logger.log(updateUserDto);
     return `This action updates a #${id} user`;
   }
 
